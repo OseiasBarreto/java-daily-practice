64ff9abe1ca552d26bcfc3bb33cf101e30925a64
Oséias Barreto <Oseiasbarreto94@gmail.com>
2025-09-23 10:58:30 -0300

    Add salary growth simulator program
    Adiciona Salary_Growth_Simulator.java, um aplicativo de console que calcula o aumento salarial com base no salário atual e no percentual desejado informado pelo usuário

---
+++ src/fundamentals/salary_growth_simulator/Salary_Growth_Simulator.java
@@ -0,0 +1,31 @@
+package fundamentals.salary_growth_simulator;
+import java.util.Scanner;
+
+public class Salary_Growth_Simulator {
+    public static void  main(String [] args) {
+
+        Scanner sc = new Scanner(System.in);
+      //  double salario;
+        //double aumento;
+
+        System.out.println("-----------------------------------------------");
+        System.out.println("Bem vindo ao simulador de crescimento salarial!");
+        System.out.println("-----------------------------------------------");
+
+        System.out.println("Informe o seu salário atual e quantos % de aumento salarial você deseja simular");
+        System.out.println("salário: ");
+        double salario = sc.nextDouble();
+
+        System.out.println("Aumento: ");
+        double aumento = sc.nextDouble();
+
+
+        System.out.println("Seu salário é: " + salario + "R$"+  " e você deseja um aumento de " + aumento + "%");
+        System.out.println("Seu aumento salarial será de: " + porcentagem(salario, aumento) + "R$");
+    }
+//metodo para calcular aumento salarial
+    public static double porcentagem (double salario, double aumento){
+        return (salario * aumento) / 100;
+    }
+
+}
